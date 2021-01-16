5776d015d0bdc3f228386c23e97513703a5ce573
CoconutMacaroon <45187468+CoconutMacaroon@users.noreply.github.com>
2021-01-16 08:25:15 -0800

    Add sample/testing code

--- Calculator.c
+++ Calculator.c
@@ -1,5 +1,14 @@
 #include "Calculator.h"
 
 int main() {
-    return 0;
+    return solveExpr("hello;");
+}
+
+int solveExpr(char* expr) {
+    char* iterator = expr;
+    while(*iterator != '\0') {
+        putchar(*iterator);
+        iterator++;
+    }
+    return 1;
 }
--- Calculator.h
+++ Calculator.h
@@ -1,1 +1,5 @@
+#include<stdio.h>
 
+int solveExpr(char* expr);
+
+const char VALID_EXPR_CHARS[] = "+-1234567890";
