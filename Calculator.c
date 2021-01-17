b7219a7cc3eccfac69a29e3bcaf4a2247c56d3df
CoconutMacaroon <45187468+CoconutMacaroon@users.noreply.github.com>
2021-01-17 10:03:23 -0800

    Add more code

--- Calculator.c
+++ Calculator.c
@@ -1,14 +1,29 @@
 #include "Calculator.h"
 
 int main() {
-    return solveExpr("hello;");
+    printf("%d", charInString('h', "hello"));
+    printf("%d", charInString('h', "test"));
+    return solveExpr("123");
 }
 
-int solveExpr(char* expr) {
-    char* iterator = expr;
-    while(*iterator != '\0') {
-        putchar(*iterator);
-        iterator++;
+int solveExpr(char *expr) {
+    // double check that the string contains only valid characters
+    char *iterator = expr;
+    while (*iterator != '\0') {
+       if(!charInString(*iterator, VALID_EXPR_CHARS)) {
+           // if it contains an invalid char, quit
+            exit(1);
+       }
+       iterator++;
     }
-    return 1;
+    return 0;
+}
+
+bool charInString(char c, char *string) {
+    while (*string != '\0') {
+        if (*string++ == c) {
+            return true;
+        }
+    }
+    return false;
 }
--- Calculator.h
+++ Calculator.h
@@ -1,5 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <assert.h>
 
-int solveExpr(char* expr);
+int solveExpr(char *expr);
 
-const char VALID_EXPR_CHARS[] = "+-1234567890";
+bool charInString(char c, char string[]);
+
+char* VALID_EXPR_CHARS = "+-1234567890";
